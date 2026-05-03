43421455b12f917321fcfd1c303d080aa38199c5
rizalmahendrag07 <71058828+RizalMahendraG07@users.noreply.github.com>
2026-05-03 23:26:57 +0700

    add vite builddd

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/', // 🔥 INI WAJIB DI RAILWAY
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    base: '/',
 });
